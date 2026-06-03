2f8ec44ec4e5fda1336f9807770b85e3315148ec
Mikhail Shishov <mdshishov@mail.ru>
2026-06-03 08:13:25 +0300

    Update users permissions

--- users/permissions.py
+++ users/permissions.py
@@ -1,17 +1,18 @@
+from django.contrib.auth import get_user_model
 from rest_framework.permissions import BasePermission
 
-from .models import CustomUser
+User = get_user_model()
 
 ROLE_TO_GROUP = {
-    CustomUser.Role.STAFF: "staff",
+    User.Role.STAFF: "staff",
 }
 
 
-class IsManager(BasePermission):
+class IsStaff(BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.is_manager
+        return request.user.is_authenticated and request.user.role == User.Role.STAFF
 
 
 class IsClient(BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.is_client
+        return request.user.is_authenticated and request.user.role == User.Role.CLIENT
