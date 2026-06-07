1e58ff2da1716b1e4638515c18e694a286a6b916
Mikhail Shishov <mdshishov@mail.ru>
2026-06-07 07:41:14 +0300

    Fix dev mod runserver

--- core/settings.py
+++ core/settings.py
@@ -32,10 +32,13 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = [
-    "cool-spot.ru",
-    "www.cool-spot.ru",
-]
+if DEBUG:
+    ALLOWED_HOSTS = ["127.0.0.1"]
+else:
+    ALLOWED_HOSTS = [
+        "cool-spot.ru",
+        "www.cool-spot.ru",
+    ]
 
 # Application definition
 
